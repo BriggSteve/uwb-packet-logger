a0cef3e1ab502511adf65c3cb751ed48e9908d42
BriggSteve <52074393+BriggSteve@users.noreply.github.com>
2025-03-28 13:04:59 -0700

    fixtimestamp overflow bug

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from uwb_packet import UWBPacket
 import time
+from uwb_packet import UWBPacket
+
+start = time.time()
 
 def mock_receive_bytes():
-    # Simulate receiving a packet every second
-    pkt = UWBPacket(device_id=1, timestamp=int(time.time()*1000), x=1.0, y=2.0, z=3.0)
+    # Relative uptime in ms to avoid overflow
+    timestamp = int((time.time() - start) * 1000)
+    pkt = UWBPacket(device_id=1, timestamp=timestamp, x=1.0, y=2.0, z=3.0)
     return pkt.to_bytes()
 
 def main():
